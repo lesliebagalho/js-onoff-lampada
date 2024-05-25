667d28d30d421ebc1f6aece387454bafe37bf60b
Leslie Bagalho <leslie.bagalho@yahoo.com.br>
2024-05-25 20:05:30 -0300

    Ligar/Desligar lâmpada com JS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+lampOff.style.display = "block";
+
+function onclickLampOn(){
+    lampOn = document.getElementById("lampOn");
+    lampOn.style.display = "block";
+
+    lampOff = document.getElementById("lampOff");
+    lampOff.style.display = "none";
+}
+
+function onclickLampOff(){
+    lampOff = document.getElementById("lampOff");
+    lampOff.style.display = "block";
+
+    lampOn = document.getElementById("lampOn");
+    lampOn.style.display = "none";
+}
+
